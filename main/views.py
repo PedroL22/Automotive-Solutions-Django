37ea45107beab9c5632893e8a7e402d07b64bdaa
Pedro L <pedrolucena@protonmail.com>
2022-07-12 18:11:37 -0300

    About Us page + navbar

--- main/views.py
+++ main/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'Home.html', {})
+
+def contact_us(request):
+    return render(request, 'Contact-Us.html', {})
+
+def about_us(request):
+    return render(request, 'About-Us.html', {})
+
+def schedule_appointment(request):
+    return render(request, 'Schedule-Appointment.html', {})
--- mechanic/urls.py
+++ mechanic/urls.py
@@ -20,4 +20,7 @@ from main import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('contact-us', views.contact_us, name='contact-us'),
+    path('about-us', views.about_us, name='about-us'),
+    path('schedule-appointment', views.schedule_appointment, name='schedule-appointment'),
 ]
