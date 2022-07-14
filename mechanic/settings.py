691f3ddff4537e1d512c73a7f45e1ecd8638e840
Pedro L <pedrolucena@protonmail.com>
2022-07-13 23:48:20 -0300

    Update settings.py

--- mechanic/settings.py
+++ mechanic/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-il2#98sw&3s4i0)7gah0t!g(+%=@z-^tubwu=p$o39#-qos4%m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://automotivesolutions.herokuapp.com/', '127.0.0.1:8000']
 
 
 # Application definition
